57eaa45d48899f7ee5d1cf5a05e55e1c1626ef7a
Nikita Ronis <78503812+DaYundexTuxi@users.noreply.github.com>
2024-10-24 11:12:35 +0300

    Add files via upload

--- sharing.js
+++ sharing.js
@@ -1,13 +1,25 @@
-function copy_route_name() {
-    var copytext = document.getElementById("route_name_input");
-
-    copytext.select();
-    copytext.setSelectionRange(0, 9999);
-
-    navigator.clipboard.writeText(copytext.value);
+function copy_link() {
+    
+    const link = window.location.href;
+    const tempInput = document.createElement('input');
+    document.body.appendChild(tempInput);
+    tempInput.value = link;
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    
+    alert('Link copied to clipboard: ' + link);
 }
 
-function copy_link() {
-    navigator.clipboard.writeText("localhost:3000/Home.html");
+function copy_route_name() {
+    const routeNameInput = document.getElementById('route_name_input');
     
+    if (routeNameInput.value.trim() !== "") {
+        routeNameInput.select();
+        document.execCommand('copy');
+        
+        alert('Route name copied to clipboard: ' + routeNameInput.value);
+    } else {
+        alert('Please enter a route name before copying.');
+    }
 }
